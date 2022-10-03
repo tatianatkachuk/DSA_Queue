99faace2c4b9041ac9103bbc728119c3b43a2595
tatianatkachuk <tatiana.tkachuk@estudiantat.upc.edu>
2022-10-03 07:58:50 +0200

    comments

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -1,28 +1,32 @@
 public class QueueImpl<E> implements Queue<E>{
+    // Declare size -- numElems
     private int numElems;
     private E[] data;
     public QueueImpl(int i) {
+        // Declare queue data of length i
         data = (E[]) new Object[i];
     }
 
     @Override
     public void push(E e) throws FullQueueException {
+        // If queue is not full, it adds he element to the end
         if (numElems>= data.length) throw new FullQueueException();
         data[numElems++] = e;
     }
 
     @Override
     public E pop() throws EmptyQueueException {
-        E e;
+        E e; // Declares element
+        // If queue is not empty
         if (numElems!= 0) {
-            e = data[0];
+            e = data[0]; // Element to delete --> then return
 
             for (int i = 0; i < numElems -1; i++)
-                data[i] = data[i + 1];
+                data[i] = data[i + 1]; // Moves elements to left
 
-            data[numElems] = null;
-            numElems--;
-            return e;
+            data[numElems] = null; // Final element is declared as null
+            numElems--; // Reduce the num of elements
+            return e; // Return the element to delete
         }
         else
             throw new EmptyQueueException();
